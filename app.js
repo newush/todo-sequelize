6adc8dd900f932759e12600f07ff93468e3be711
newush <ushneway@gmail.com>
2021-01-30 23:48:35 -0500

    feat: add message partials and isAuthenticated to display different view

--- app.js
+++ app.js
@@ -4,24 +4,34 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const usePassport = require('./config/passport')
-
+const flash = require('connect-flash')
 const routes = require('./routes')
 const app = express()
-usePassport(app)
 
 const PORT = 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 app.use(session({
   secret: 'ThisIsMySecret',
   saveUninitialized: true,
   resave: false
 }))
+usePassport(app) //這個位置放錯 找了兩個小時 驗證一直失敗
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  res.locals.error = req.flash('error')
+  next()
+})
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
+
+
 
 app.use(routes)
 app.listen(PORT, () => {
--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,7 @@ const User = db.User
 module.exports = app => {
   app.use(passport.initialize())
   app.use(passport.session())
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+  passport.use(new LocalStrategy({ usernameField: 'email', passReqToCallback: true }, (req, email, password, done) => {
     User.findOne({ where: { email } })
       .then(user => {
         if (!user) {
